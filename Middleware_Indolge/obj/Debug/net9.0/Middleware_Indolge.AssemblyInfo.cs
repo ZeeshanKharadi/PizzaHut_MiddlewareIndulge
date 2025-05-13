67ffd401db89e23b26eb383b35086e528ab48b4c
ZeeshanKharadi <zeeshan.kharadi11@gmail.com>
2025-05-13 12:47:56 +0500

    .

--- Middleware_Indolge/obj/Debug/net9.0/Middleware_Indolge.AssemblyInfo.cs
+++ Middleware_Indolge/obj/Debug/net9.0/Middleware_Indolge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Middleware_Indolge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+850c40846f3fa4eee625f67e885e1fbe54778fb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95402111cc9f6d827eeb83d757a9ae2e9fe04b5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Middleware_Indolge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Middleware_Indolge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
